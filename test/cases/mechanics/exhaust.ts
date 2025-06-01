eb56a23a75ab9cafe29bffe1f0515a8ff7e52600
Mario Torres <mariotorresdev@gmail.com>
2025-06-01 17:51:57 -0500

    fix: added more Mythosaur cases

--- test/cases/mechanics/exhaust.ts
+++ test/cases/mechanics/exhaust.ts
@@ -40,5 +40,73 @@ export const ExhaustCases = {
       .MyGroundUnitIsExhausted(1)
       .RunAsync()
     ;
+  },
+  Mythosaur_ally_NGTMD_after_attack: async function () {
+    //arrange
+    const gameState = new GameState(gameName);
+    await gameState.LoadGameStateLinesAsync();
+    await gameState.ResetGameStateLines()
+      .SetBasesDamage("1 0")
+      .AddBase(1, cards.generic.BlueBase)
+      .AddLeader(1, cards.SOR.PalpLeader)
+      .AddBase(2, cards.generic.BlueBase)
+      .AddLeader(2, cards.SHD.CadBaneLeader)
+      .FillResources(1, cards.SOR.BFMarine, 2)
+      .FillResources(2, cards.SOR.BFMarine, 2)
+      .AddCardToDeck(1, cards.SOR.InfernoFour, 2)
+      .AddCardToDeck(2, cards.SOR.CraftySmuggler, 2)
+      .AddCardToHand(2, cards.SOR.NGTMD)
+      .AddUnitWithUpgrade(1, cards.LOF.Mythosaur, cards.generic.Shield)
+      .FlushAsync(com.BeginTestCallback)
+    ;
+    //act
+    const gameplay = new GamePlay(browser);
+    await gameplay
+      .WaitForPassButton().PassTurn()
+      .SwitchPlayerWindow().WaitForMyHand().PlayFromHand(1).TargetTheirGroundUnit(1)
+      .SwitchPlayerWindow().WaitForMyGroundUnit(1).AttackWithMyGroundUnit(1)
+      .SwitchPlayerWindow().WaitForClaimButton().ClaimInitiative()
+      .SwitchPlayerWindow().WaitForPassButton().PassTurn()
+      .SwitchPlayerWindow().WaitForMyHand().ClickHandCard(1)
+      .SwitchPlayerWindow().WaitForMyHand().ClickHandCard(1)
+      .RunAsync()
+    ;
+    //assert
+    gameplay.Assert()
+      .MyGroundUnitIsExhausted(1)
+      .RunAsync()
+    ;
+  },
+  Mythosaur_can_still_exhaust_friendly_unit: async function () {
+    //arrange
+    const gameState = new GameState(gameName);
+    await gameState.LoadGameStateLinesAsync();
+    await gameState.ResetGameStateLines()
+      .SetBasesDamage("1 0")
+      .AddBase(1, cards.generic.BlueBase)
+      .AddLeader(1, cards.SOR.PalpLeader)
+      .AddBase(2, cards.generic.GreenBase)
+      .AddLeader(2, cards.SOR.SabineLeader)
+      .FillResources(1, cards.SOR.BFMarine, 2)
+      .FillResources(2, cards.SOR.BFMarine, 2)
+      .AddCardToHand(1, cards.SOR.ColonelYularen)
+      .AddUnitWithUpgrade(1, cards.TWI.PoggleTheLesser, cards.generic.Shield)
+      .AddUnit(1, cards.LOF.Mythosaur)
+      .FlushAsync(com.BeginTestCallback)
+    ;
+    //act
+    const gameplay = new GamePlay(browser);
+    await gameplay
+      .WaitForMyHand().PlayFromHand(1).Pass().ChooseYes()
+      .RunAsync()
+    ;
+    //assert
+    gameplay.Assert()
+      .MyHandIsEmpty()
+      .MyBaseDamageEquals('0')
+      .MyGroundUnitIsBattleDroid(4)
+      .MyGroundUnitIsExhausted(1)
+      .RunAsync()
+    ;
   }
 }
