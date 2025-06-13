55a02f6bf71751b124aa863f3e94354d998121e2
Mario Torres <mariotorresdev@gmail.com>
2025-06-13 15:05:19 -0500

    fix(init): added delay to deck link paste

--- test/utils/helper-func.ts
+++ test/utils/helper-func.ts
@@ -30,7 +30,7 @@ export async function NewRunGameStartAsync(browser: NightwatchAPI) {
 
   await browser
     .waitForElementPresent(com.DeckInput)
-    .setValue(com.DeckInput, swustatsDeck)
+    .setValue(com.DeckInput, swustatsDeck).pause(p.WaitForEffect)
     .click(com.CreateGameButton).pause(p.ButtonPress)
     .refresh().pause(p.ButtonPress)
     .refresh().pause(p.WaitToBegin)
